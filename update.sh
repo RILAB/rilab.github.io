98b8b1f277f58efe5744db8fec942db6a9d9a5f3
Jeffrey Ross-Ibarra <rossibarra@gmail.com>
2014-05-17 13:52:22 -0700

    update

--- update.sh
+++ update.sh
@@ -15,6 +15,7 @@ for i in *.md;
 do
 	pandoc -S -s -c ../pandoc.css -H ../header.html -o ./${i//md/html} $i	
 	#pandoc -S -s -c ../pygment_trac.css -c ../styles.css -o ./${i//md/html} $i	
+	git add ./${i//md/html}
 done
 
 git commit -am "update"; 
