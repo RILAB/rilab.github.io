d4fa408828ba0ae8c218018244431910b1696e64
Jeffrey Ross-Ibarra <rossibarra@gmail.com>
2014-05-17 13:49:58 -0700

    update

--- update.sh
+++ update.sh
@@ -8,5 +8,14 @@ do
 	#pandoc -S -s -c ../pygment_trac.css -c ../styles.css -o ../${i//md/html} $i	
 done
 
+
+cd ../blogs
+
+for i in *.md; 
+do
+	pandoc -S -s -c ../pandoc.css -H ../header.html -o ../${i//md/html} $i	
+	#pandoc -S -s -c ../pygment_trac.css -c ../styles.css -o ../${i//md/html} $i	
+done
+
 git commit -am "update"; 
 git push
