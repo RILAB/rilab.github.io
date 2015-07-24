8f3fcf03da0cf624a05580f598cdf03fe251f8f0
Jeff Ross-Ibarra <rossibarra@gmail.com>
2015-07-24 13:17:32 -0700

    update

--- update_web.pl
+++ update_web.pl
@@ -25,7 +25,7 @@ while(<FILE>){
 		$_=~m/CITES:(\S+)/;
 		my $tempid=$1; 
 		my $citecount=0;
-		foreach(keys(%cites)){ next if $_=="NA"; $citecount=$cites{$tempid}; }
+		foreach(keys(%cites)){ next if $_ eq "NA"; $citecount=$cites{$tempid}; }
 			
 		$_=~s/CITES:\S+/[[$citecount citations](http:\/\/scholar.google.com\/scholar?oi=bibs&hl=en&cites=$tempid )]/;
 	}
