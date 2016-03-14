d4bfc2e1554ad9fc0397e675564613f7247e17b8
Jeff Ross-Ibarra <rossibarra@gmail.com>
2016-03-14 00:04:42 -0700

    update

--- h.r
+++ h.r
@@ -1,7 +1,7 @@
 # echo $( rscript --vanilla  h.r ) | sed -e 's/\[1\]//g;s/\"//g'
 
 library(scholar)
-bob<-get_profile("5SzRq1oAAAAJ")
-paste("{\\small H-Index ", bob$h_index, " (", bob$total_cites, " citations as of ", date(), ")}",sep="")
-bob<-get_publications("5SzRq1oAAAAJ")
-write.table(file="./tempcites.txt",data.frame(bob$cid,bob$cites),row.names=F,quote=F,col.names=F)
+bob<-as.list(get_profile("5SzRq1oAAAAJ"))
+#paste("{\\small H-Index ", bob$h_index, " (", bob$total_cites, " citations as of ", date(), ")}",sep="")
+#sue<-get_publications("5SzRq1oAAAAJ")
+#write.table(file="./tempcites.txt",data.frame(sue$cid,sue$cites),row.names=F,quote=F,col.names=F)
