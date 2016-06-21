854a01230c4aa27c8c49d00216270ed11848ea4b
Jeff Ross-Ibarra <rossibarra@gmail.com>
2016-06-21 01:17:39 -0700

    always use library(methods) for Rscript

--- h.r
+++ h.r
@@ -1,6 +1,7 @@
 # echo $( rscript --vanilla  h.r ) | sed -e 's/\[1\]//g;s/\"//g'
 
 library(scholar)
+library(methods)
 bob<-get_profile("5SzRq1oAAAAJ")
 paste("{\\small H-Index ", bob$h_index, " (", bob$total_cites, " citations as of ", date(), ")}",sep="")
 sue<-get_publications("5SzRq1oAAAAJ")
